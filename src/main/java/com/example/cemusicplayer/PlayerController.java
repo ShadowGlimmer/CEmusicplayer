abfac89d7fe062df87d9b2410dddb2d2434ba32c
Specktic <111095859+Specktic@users.noreply.github.com>
2022-08-26 23:23:39 -0600

    create player window pane

--- src/main/java/com/example/cemusicplayer/PlayerController.java
+++ src/main/java/com/example/cemusicplayer/PlayerController.java
@@ -1,14 +1,32 @@
 package com.example.cemusicplayer;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
 
-public class PlayerController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlayerController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Pane playPane;
+    @FXML
+    private Label songPlayingLabel, artistLabel, genreLabel;
+    @FXML
+    private Button playButton, nextButton, previousButton, continuousButton;
+    @FXML
+    private Slider songVolumeSlider;
+    @FXML
+    private ProgressBar songProgressBar;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
