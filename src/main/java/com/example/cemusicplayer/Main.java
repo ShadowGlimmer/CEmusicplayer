0e68b74e29b498ab7ba218571eb97f297212b6a4
ShadowGlimmer <josechunche@hotmail.com>
2022-09-06 19:08:55 -0500

    Basic test functionality for login screen and access to music player screen.

--- src/main/java/com/example/cemusicplayer/Main.java
+++ src/main/java/com/example/cemusicplayer/Main.java
@@ -2,22 +2,30 @@ package com.example.cemusicplayer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("player-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle(" ");
-        stage.setScene(scene);
-        stage.show();
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("login-view.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+    }
+
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/com/example/cemusicplayer/PlayerController.java
+++ src/main/java/com/example/cemusicplayer/PlayerController.java
@@ -1,29 +1,50 @@
 package com.example.cemusicplayer;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.Pane;
 
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PlayerController implements Initializable {
 
+    private Stage stage;
+    private Scene scene;
     private boolean paused = true;
+
     @FXML
     private Pane playPane;
     @FXML
     private Label songPlayingLabel, artistLabel, genreLabel;
     @FXML
-    private Button playButton, nextButton, previousButton, continuousButton;
+    private Button playButton, nextButton, previousButton, continuousButton, loginButton;
     @FXML
     private Slider songVolumeSlider;
     @FXML
     private ProgressBar songProgressBar;
+
+
+    public void EnterMusicPlayer(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("player-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public void playSong(){
         if (paused){
@@ -38,6 +59,5 @@ public class PlayerController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 }
